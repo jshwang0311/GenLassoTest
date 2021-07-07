fcc5b1052fe42e818b7c79a29844c645781f265e
postechiminuru@gmail.com <postechiminuru@gmail.com>
2021-07-08 00:09:41 +0900

    initial commit

---
+++ R/genlasso_path_ftn.R
@@ -0,0 +1,173 @@
+#' Get Exact Solution Path for genlasso problem.
+#'
+#' This function solves the genlasso problem for all lambda.
+#'
+#' @param y a numeric response vector.
+#' @param X a matrix of predictor variables.
+#' @param D a penalty matrix.
+#' @param genlasso.option whether to use the genlasso package.
+#' @return solution path object
+#' @export
+#'
+ESPgenlasso <- function(y, X, D, genlasso.option=F, thres.lambda = 0.1, iter = 100000, tol=1e-12)
+{
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  m <- dim(D)[1]
+  lbd <- c()
+  bd.set <- c()
+  sgn.set <- c()
+  delta <- c()
+  lbd[1] <- .Machine$double.xmax
+  inv.x <- .ginv_ftn(t(X)%*%(X),tol)%*%t(X)
+  til.d <- D%*%inv.x
+  til.y <- X%*%inv.x%*%y
+  if(n<p){
+    X1 <- rbind(X,matrix(rep(0,(p-n)*p),ncol=p))
+    w.mat <- matrix(.null_ftn(t(X1),tol),nrow=dim(X1)[2])
+  }
+  if(n>=p){
+    w.mat <- matrix(.null_ftn(t(X),tol),nrow=dim(X)[2])
+  }
+  null.size <- dim(w.mat)[2]
+  if(null.size==0){
+    pack.genlasso.rslt <- genlasso(y, X, D)
+    return(list(u = pack.genlasso.rslt$u,delta = null,delta_slope = null, lambda=pack.genlasso.rslt$lambda, hit=pack.genlasso.rslt$hit, beta=pack.genlasso.rslt$beta, fit=pack.genlasso.rslt$fit))
+  } else if(genlasso.option){
+    pack.genlasso.rslt <- genlasso(y, X, D,svd=T,eps=1e-8)
+    return(list(u = pack.genlasso.rslt$u,delta = null,delta_slope = null, lambda=pack.genlasso.rslt$lambda, hit=pack.genlasso.rslt$hit, beta=pack.genlasso.rslt$beta, fit=pack.genlasso.rslt$fit))
+  } else{
+    u.mat <- matrix(nrow=iter,ncol=m)
+    delta.mat <- matrix(nrow = iter,ncol=null.size)
+    delta.mat.slope <- matrix(nrow = iter,ncol=null.size)
+    bd <- list()
+    sgn <- list()
+    solv.temp <- list()
+    hit <- c()
+
+    for(k in c(1:iter)){
+      if(k==1){
+        bd[[k]] <- bd.set
+        sgn[[k]] <- sgn.set
+        inv.mat <- .gen_mat_ftn(D,inv.x,w.mat,bd.set,tol)
+        solv.mat <- (inv.mat%*%rbind((til.d%*%til.y),as.matrix(rep(0,null.size))))
+        solv.temp[[k]] <- solv.mat
+
+        u.mat[k,] <- solv.mat[1:m,]
+        delta.mat[k,] <- solv.mat[c((dim(solv.mat)[1]-null.size + 1):(dim(solv.mat)[1])),]
+        delta <- delta.mat[k,]
+        lbd[k] <- max(abs(u.mat[k,]))
+        bd.set <- which(abs(abs(u.mat[k,])-lbd[k])<tol)
+        sgn.set <- sign(u.mat[k,bd.set])
+        r.delta <- delta
+        hit <- T
+      } else{
+        bd[[k]] <- bd.set
+        sgn[[k]] <- sgn.set
+        bf.u <- u.mat[(k-1),]
+        bf.lbd <- lbd[k-1]
+        int.size <- m-length(bd.set)
+        int.set <- c(1:m)[-bd.set]
+
+        if(length(bd.set)==m){
+          delta <- r.delta
+          delta.mat[k,] <- delta
+          slope <- 0
+          delta.slope <- 0
+          delta.mat.slope[k-1,] <- rep(0,null.size)
+          lv.rslt <- .lv_ftn(bf.u,bf.lbd,til.y,m,D,til.d,w.mat,slope,delta,delta.slope,bd.set,sgn.set)
+          if(lv.rslt$lbd==0){
+            lbd[k] <- 0
+            break
+          } else{
+            lbd[k] <- lv.rslt$lbd
+            u.mat[k,-bd.set] <- bf.u[-bd.set] + (lbd[k] - bf.lbd)*slope
+            u.mat[k,bd.set] <- lbd[k]*sgn.set
+
+            bf.u <- u.mat[(k),]
+            bf.lbd <- lbd[k]
+            int.size <- m-length(bd.set)
+
+            sgn.set <- sgn.set[-which(bd.set==lv.rslt$coord)]
+            bd.set <- bd.set[-which(bd.set==lv.rslt$coord)]
+            hit <- c(hit,F)
+            r.delta <- delta + (lbd[k]-bf.lbd)*delta.slope
+          }
+        } else{
+          delta <- r.delta
+          delta.mat[k,] <- delta
+
+          temp.mat <- cbind((matrix(til.d[-bd.set,],ncol=dim(til.d)[2])%*%t(matrix(til.d[-bd.set, ], ncol=dim(til.d)[2]))),
+                            (D[-bd.set,]%*%w.mat))
+          temp.mat <- rbind(temp.mat, cbind(t((D[-bd.set,]%*%w.mat)), matrix(rep(0,(null.size*null.size)),nrow=null.size)))
+
+          inv.mat <- .svdsolve(temp.mat,tol)
+
+          mid.mat1 <- -1*matrix(til.d[-bd.set,],ncol=dim(til.d)[2])%*%(t(matrix(til.d[bd.set,],ncol=dim(til.d)[2]))%*%matrix(sgn.set,ncol=1))
+          mid.mat2 <- -1*t(matrix(D[bd.set,],ncol=dim(D)[2])%*%w.mat)%*%matrix(sgn.set,ncol=1)
+          mid.mat <- rbind(mid.mat1,mid.mat2)
+
+          rslt.mat <- inv.mat%*%mid.mat
+
+
+          slope <- rslt.mat[c(1:int.size),]
+          delta.slope <- rslt.mat[c((int.size+1):dim(rslt.mat)[1]),]
+          delta.mat.slope[k-1,] <- delta.slope
+
+          if(hit[length(hit)]==F){
+            lv.coord.slope <- slope[which(int.set==lv.rslt$coord)]
+            lv.coord.sign <- sign(bf.u[lv.rslt$coord])
+            if(lv.coord.slope*lv.coord.sign<1){
+              break
+            }
+          }
+          hit.rslt <- .hit_ftn(bf.u,bf.lbd,slope,bd.set,m)
+          lv.rslt <- .lv_ftn(bf.u,bf.lbd,til.y,m,D,til.d,w.mat,slope,delta,delta.slope,bd.set,sgn.set)
+
+          if(hit.rslt$lbd>lv.rslt$lbd){
+            lbd[k] <- hit.rslt$lbd
+            u.mat[k,-bd.set] <- bf.u[-bd.set] + (lbd[k] - bf.lbd)*slope
+            u.mat[k,bd.set] <- lbd[k]*sgn.set
+
+            sgn.set <- c(sgn.set,sign(u.mat[k,hit.rslt$coord[1]]))
+            bd.set <- c(bd.set,hit.rslt$coord[1])
+
+            u.mat[k,bd.set] <- lbd[k]*sgn.set
+            r.delta <- delta + (lbd[k]-bf.lbd)*delta.slope
+            hit <- c(hit,T)
+          } else{
+            if(lv.rslt$lbd==0){
+              lbd[k] <- 0
+              break
+            }
+            lbd[k] <- lv.rslt$lbd
+            u.mat[k,-bd.set] <- bf.u[-bd.set] + (lbd[k] - bf.lbd)*slope
+            u.mat[k,bd.set] <- lbd[k]*sgn.set
+
+            bf.u <- u.mat[(k),]
+            bf.lbd <- lbd[k]
+
+            sgn.set <- sgn.set[-which(bd.set==lv.rslt$coord)]
+            bd.set <- bd.set[-which(bd.set==lv.rslt$coord)]
+            hit <- c(hit,F)
+            r.delta <- delta + (lbd[k]-lbd[k-1])*delta.slope
+          }
+        }
+      }
+      if(lbd[k]<=thres.lambda){
+        break
+      }
+    }
+    u.mat <- matrix(u.mat[!is.na(u.mat[,1]),],ncol=m)
+    delta.mat <- matrix(delta.mat[!is.na(delta.mat[,1]),],ncol=null.size)
+    delta.mat.slope <- matrix(delta.mat.slope[!is.na(delta.mat.slope[,1]),],ncol=null.size)
+
+    delta.mat[which(abs(delta.mat)<tol)] <- 0
+    delta.mat.slope[which(abs(delta.mat.slope)<tol)] <- 0
+    rownames(u.mat) <- apply(u.mat,1,function(x) max(abs(x)))
+    beta <- .ginv_ftn(t(X) %*% X, tol) %*% (matrix(rep(t(X) %*% y, times=length(lbd)), dim(X)[2], length(lbd)) - t(D) %*% t(u.mat)) - w.mat %*% (t(delta.mat)[,-1])
+    fit <- X %*% beta
+    return(list(u = t(u.mat), delta = (delta.mat), delta_slope = delta.mat.slope,
+                lambda = lbd, hit = hit, beta = beta, fit = fit))
+  }
+}
---
+++ R/utils.R
@@ -0,0 +1,202 @@
+.ginv_ftn <- function(X, tol = sqrt(.Machine$double.eps))
+{
+  ## Generalized Inverse of a Matrix
+  dnx <- dimnames(X)
+  if(is.null(dnx)) dnx <- vector("list", 2)
+  s <- svd(X)
+  nz <- s$d > tol * s$d[1]
+  if(any(nz)){
+    rslt <- s$v[, nz] %*% (t(s$u[, nz])/s$d[nz])
+  }
+  else{
+    rslt <- X
+  }
+  return(rslt)
+}
+
+.null_ftn <- function(M,tol=sqrt(.Machine$double.eps))
+{
+  tmp <- svd(M)
+  ind <- which(abs(tmp$d) <tol)
+  rslt <- tmp$u[,ind]
+  return(rslt)
+}
+
+
+.hit_ftn <- function(bf.u,bf.lbd,slope,bd.set,m)
+{
+  if(length(bd.set)==0){
+    int.set <- c(1:m)
+  }
+  else{
+    int.set <- c(1:m)[-bd.set]
+  }
+  over.part <- bf.u[int.set] - slope*bf.lbd
+
+  under.part1 <- 1-slope
+  under.part2 <- -1-slope
+
+  rslt1 <- over.part/under.part1
+  rslt2 <- over.part/under.part2
+
+  hit.rslt <- rep(0,length(int.set))
+
+  for(i in 1:length(rslt1))
+  {
+    if(rslt1[i]>0 && rslt2[i]>0)
+    {
+      if(rslt1[i] > rslt2[i]){
+        rslt1[i] <- -1
+      }
+      else{
+        rslt2[i] <- -1
+      }
+    }
+  }
+
+  hit.rslt[which(rslt1>0)] <- rslt1[which(rslt1>0)]
+  hit.rslt[which(rslt2>0)] <- rslt2[which(rslt2>0)]
+  hit.rslt[which(hit.rslt>bf.lbd)] <- bf.lbd
+
+  return(list(lbd=max(hit.rslt),coord = int.set[which.max(hit.rslt)]))
+}
+
+.lv_ftn <- function(bf.u,bf.lbd,til.y,m,l.d,til.d,w.mat,slope,delta,delta.slope,bd.set,sgn.set)
+{
+  int.size <- m - length(bd.set)
+  null.size <- dim(w.mat)[2]
+  tol=sqrt(.Machine$double.eps)
+  if(int.size>0){
+    f.part <- matrix(til.d[bd.set,],ncol=dim(til.d)[2])%*%(
+      matrix(til.y,ncol=1) - t(matrix(til.d[-bd.set,],ncol=dim(til.d)[2]))%*%matrix(bf.u[-bd.set],ncol=1) +
+        bf.lbd*t(matrix(til.d[-bd.set,],ncol=dim(til.d)[2]))%*%matrix(slope,ncol=1)) - ((l.d[bd.set,]%*%w.mat))%*%(matrix(delta,ncol=1) -
+                                                                                                                     bf.lbd*matrix(delta.slope,ncol=1))
+    s.part <- matrix(til.d[bd.set,],ncol=dim(til.d)[2])%*%(
+      t(matrix(til.d[bd.set,],ncol=dim(til.d)[2]))%*%matrix(sgn.set,ncol=1) +
+        t(matrix(til.d[-bd.set,],ncol=dim(til.d)[2]))%*%matrix(slope,ncol=1)) + ((l.d[bd.set,]%*%w.mat))%*%matrix(delta.slope,ncol=1)
+
+    f.part <- f.part * sgn.set
+    s.part <- s.part * sgn.set
+  }
+  else{
+    d.bd <- matrix(l.d[bd.set,],ncol=dim(l.d)[2])
+    til.d.bd <- matrix(til.d[bd.set,],ncol=dim(til.d)[2])
+    f.part <- til.d.bd%*%til.y - d.bd%*%w.mat%*%delta
+    f.part <- f.part*sgn.set
+    s.part <- til.d.bd%*%t(til.d.bd)%*%sgn.set
+    s.part <- s.part*sgn.set
+  }
+
+  temp.lbd <- c(0)
+  temp.coord <- c(1)
+  f.part[which(abs(f.part)<tol)] <- 0
+  s.part[which(abs(s.part)<tol)] <- 0
+
+  for(i in 1:length(bd.set))
+  {
+    if(f.part[i]<0){
+      if(s.part[i]<0){
+        if(temp.lbd < f.part[i]/s.part[i]){
+          temp.lbd <- f.part[i]/s.part[i]
+          temp.coord <- i
+        }
+      }
+    }
+  }
+  if(temp.lbd>bf.lbd){
+    temp.lbd <- bf.lbd
+  }
+  return(list(lbd=temp.lbd,coord=bd.set[temp.coord]))
+}
+
+.mult_element_ftn <- function(A.mat, B.mat){
+  for(i in c(1:dim(A.mat)[1]))
+  {
+    B.mat[i,] <- B.mat[i,]*A.mat[i,]
+  }
+  return(B.mat)
+}
+
+
+.gen_mat_ftn <- function(l.d,inv.x,w.mat,bd.set,tol)
+{
+  til.d <- l.d%*%inv.x
+  null.size <- dim(w.mat)[2]
+  if(length(bd.set)>0){
+    temp.mat <- cbind((matrix(til.d[-bd.set,],ncol=dim(til.d)[2])%*%t(matrix(til.d[-bd.set,],ncol=dim(til.d)[2]))),(l.d[-bd.set,]%*%w.mat))
+    temp.mat <- rbind(temp.mat,cbind(t((l.d[-bd.set,]%*%w.mat)),matrix(rep(0,(null.size*null.size)),nrow=null.size)))
+  }
+  else{
+    temp.mat <- cbind((til.d%*%t(til.d)),(l.d%*%w.mat))
+    temp.mat <- rbind(temp.mat,cbind(t((l.d%*%w.mat)),matrix(rep(0,(null.size*null.size)),nrow=null.size)))
+  }
+  if(dim(temp.mat)[1]>10000){
+    inv.mat <- svdsolve(temp.mat,5000,tol,T)$x
+  }
+  else{
+    inv.mat <- tryCatch({
+      inv.mat <- svdsolve(temp.mat,5000,tol,F)$x
+    }, error = function(err) {
+      err$message = paste(err$message, "\n(svd solve using package)", sep = "")
+      warning(err)
+      inv.mat <- svdsolve(temp.mat,min(rankMatrix(temp.mat)[1],5000),tol,T)$x
+      return(inv.mat)
+    })
+  }
+  return(inv.mat)
+}
+
+.svdsolve_ftn <- function(A,nu,rtol = 1e-5,pack.use=F,propack=F) {
+  if(pack.use){
+    if(propack){
+      s= svd::propack.svd(A)
+    }
+    else{
+      maxit = 5
+      s= irlba::irlba(A,nu,tol=1e-5,maxit = maxit)
+      for(i in c(1:(nu))){
+        if(s$iter==maxit){
+          s= irlba::irlba(A,nu-i,tol=1e-5,maxit = maxit)
+        }
+        else{
+          break
+        }
+      }
+    }
+  }
+  else{
+    s = svd(A)
+  }
+  di = s$d
+  ii = di>rtol
+  di[ii] = 1/di[ii]
+  di[!ii] = 0
+  return(list(x=s$v%*%(di*(t(s$u))),q=sum(ii)))
+}
+
+.svdsolve <- function(A,tol){
+  if(dim(A)[1]>10000){
+    inv.mat <- .svdsolve_ftn(A,5000,tol,T,F)$x
+  }
+  else{
+    inv.mat <- tryCatch({
+      inv.mat <- .svdsolve_ftn(A,5000,tol,F,F)$x
+    }, error = function(err) {
+      err$message <- paste(err$message, "\n(svd solve using package)", sep = "")
+      warning(err)
+      inv.mat <- tryCatch({
+        inv.mat <- .svdsolve_ftn(A, (Matrix::rankMatrix(A)[1]), tol, T, F)$x
+        #inv.mat <- .svdsolve_ftn(A,tol,T,T)$x
+      },error = function(err) {
+        err$message <- paste(err$message, "\n(svd solve using package)", sep = "")
+        warning(err)
+        inv.mat <- .svdsolve_ftn(A, tol, T, T)$x
+        #inv.mat <- .svdsolve_ftn(A,(Matrix::rankMatrix(A)[1]),tol,T,F)$x
+        return(inv.mat)
+      }
+      )
+      return(inv.mat)
+    })
+  }
+  return(inv.mat)
+}
